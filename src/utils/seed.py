15de00be9fecaf9d7c9ec0db60dc9d9d51082047
qianmo-DUlI <482005684@qq.com>
2026-06-20 18:51:28 +0800

    seed设置

---
+++ src/utils/seed.py
@@ -0,0 +1,8 @@
+#在测试中固定种子
+# seed = 42
+# random.seed(seed)
+# env.action_space.seed(seed=seed)
+# env.observation_space.seed(seed=seed)
+# torch.manual_seed(seed)
+# if torch.cuda.is_available():
+#     torch.cuda.manual_seed(seed)
